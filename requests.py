31b0865fe6cdcdab0e838bb06deea8a0518321fc
Balaji45-Ncr <ncrlpb45@gmail.com>
2024-08-13 22:00:49 +0530

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,8 @@
+import requests
+url="https://www.weather.reports.up.com"
+response=requests.get(url)
+if requests.status_code==200:
+  print(response.json())
+else:
+  print(f'{requests.status_code} is not working please check it again')
+  
